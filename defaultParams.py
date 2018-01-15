8481f5624a811e97f9aacd807df397329b2c47b1
zlipp3032 <zlipp3032@gmail.com>
2018-01-15 18:01:57 -0500

    currently set up to be used in both Flight Programs

--- defaultParams.py
+++ defaultParams.py
@@ -10,3 +10,5 @@ defaultParams.Ts = 0.05#20Hz
 defaultParams.expectedMAVs = 1
 defaultParams.kp = 5 # Proportional gain for PD controller to follow leader
 defaultParams.kd = 0.9 # Derivative gain for PD controller to follow leader
+defaultParams.GPSTimeout = 1.0
+defaultParams.isComplete = True #Ensure the Default parameters were loaded correctly into the program
