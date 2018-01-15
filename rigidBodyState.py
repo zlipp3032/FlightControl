f7444d19bbae0e11d427dbda9322dd956896c130
zlipp3032 <zlipp3032@gmail.com>
2018-01-15 18:01:00 -0500

    Can be used in both main flight programs

--- rigidBodyState.py
+++ rigidBodyState.py
@@ -11,9 +11,8 @@ Velocity = recordtype('Velocity',[('vx',0),('vy',0),('vz',0)], default = None)
 ComputeControl = recordtype('ComputerControl',[('ux',0),('uy'),('uz',0)], default = None)
 Leader = recordtype('Leader',[('qgx',0),('qgy',0),('qgz',0),('pgx',0),('pgy',0),('pgz',0)], default = None)
 
-Parameter = recordtype('Parameter',['Ts','peerTimeout','expectedMAVs','kp','kd'], default = None)
+Parameter = recordtype('Parameter',['Ts','peerTimeout','GPSTimeout','expectedMAVs','kp','kd','isComplete'], default = None)
 
-RigidBodyState = recordtype('RigidBodyState', [('startTime', None),'ID','time','attitude',('position',Position()),('velocity',Velocity()),('heading',0.0),('command',Command()),('parameters',Parameter()),('timeout',Timeout()),('control',ComputeControl()),('leader',Leader())], default = None)
+RigidBodyState = recordtype('RigidBodyState', [('startTime', None),'ID','time','attitude',('RCLatch',False),('isGPS',False),('position',Position()),('velocity',Velocity()),('heading',0.0),('command',Command()),('parameters',Parameter()),('timeout',Timeout()),('control',ComputeControl()),('leader',Leader())], default = None)
 
 Message = recordtype('Message', 'type,sendTime,content', default = None)
-
