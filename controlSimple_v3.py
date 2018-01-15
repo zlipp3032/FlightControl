11bc6691530f9922bedfa87d1675bdc40a03475d
zlipp3032 <zlipp3032@gmail.com>
2018-01-15 15:27:28 -0500

    Still need to properly scale commands and implement fail safes

--- controlSimple_v3.py
+++ controlSimple_v3.py
@@ -77,9 +77,11 @@ class Control(threading.Thread):
     def getRigidBodyState(self,msg):
         self.rigidBodyState.timeout.peerLastRx[msg.content.ID] = datetime.now() #This might cause issues the way this is currently set up...need to fix...
 #        self.rigidBodyState.position = self.RigidBodies[ID]
-        self.rigidBodyState.position = msg.content.position
-        self.rigidBodyState.velocity = msg.content.velocity
-        self.rigidBodyState.leader = msg.content.leader
+        if(msg.content.ID>0):
+            self.rigidBodyState.position = msg.content.position
+            self.rigidBodyState.velocity = msg.content.velocity
+        else:
+            self.rigidBodyState.leader = msg.content.leader
 #        print self.rigidBodyState.position
         self.rigidBodyState.time = datetime.now()
         self.counter+=1
@@ -104,20 +106,36 @@ class Control(threading.Thread):
         self.logQueue.put(msg)
 
     def computeControl(self):
-        self.rigidBodyState.control.ux = -self.rigidBodyState.parameters.kp*(self.rigidBodyState.position.x - self.rigidBodyState.leader.qgx) - self.rigidBodyState.parameters.kd*(self.rigidBodyState.velocity.vx - self.rigidBodyState.leader.pgx)
-        self.rigidBodyState.control.uy = -self.rigidBodyState.parameters.kp*(self.rigidBodyState.position.y - self.rigidBodyState.leader.qgy) - self.rigidBodyState.parameters.kd*(self.rigidBodyState.velocity.vy - self.rigidBodyState.leader.pgy)
-        self.rigidBodyState.control.uz = -self.rigidBodyState.parameters.kp*(self.rigidBodyState.position.z - self.rigidBodyState.leader.qgz) - self.rigidBodyState.parameters.kd*(self.rigidBodyState.velocity.vz - self.rigidBodyState.leader.pgz)
+        self.rigidBodyState.control.ux = self.rigidBodyState.parameters.kp*(self.rigidBodyState.position.x - self.rigidBodyState.leader.qgx) + self.rigidBodyState.parameters.kd*(self.rigidBodyState.velocity.vx - self.rigidBodyState.leader.pgx)
+        self.rigidBodyState.control.uy = self.rigidBodyState.parameters.kp*(self.rigidBodyState.position.y - self.rigidBodyState.leader.qgy) + self.rigidBodyState.parameters.kd*(self.rigidBodyState.velocity.vy - self.rigidBodyState.leader.pgy)
+        self.rigidBodyState.control.uz = self.rigidBodyState.parameters.kp*(self.rigidBodyState.position.z - self.rigidBodyState.leader.qgz) + self.rigidBodyState.parameters.kd*(self.rigidBodyState.velocity.vz - self.rigidBodyState.leader.pgz)
 #        print self.rigidBodyState.control
         #Implement channel override algorithm here
         self.scaleControl()
 
     def scaleControl(self):
+        #Scale the compute control values to match the format used in vehicle.channel.overrides{}
         ROLL = 1
         PITCH = 2
         THROTTLE = 3 
-        YAW = 4
+        YAW = 1020
+        # Saturate to keep commands in range of input values
+        ROLL = self.saturate(ROLL,1000,2000)
+        PITCH = self.saturate(PITCH,1000,2000)
+        THROTTLE = self.saturate(THROTTLE,1000,2000)
+        YAW = self.saturate(YAW,1000,2000)
         self.sendCommand(ROLL,PITCH,THROTTLE,YAW)
 
     def sendCommand(self,ROLL,PITCH,THROTTLE,YAW):
-        print 'Roll: %s, Pitch: %s, Throttle: %s, Yaw,%s' % (ROLL,PITCH,THROTTLE,YAW)
+#        print 'Roll: %s, Pitch: %s, Throttle: %s, Yaw,%s' % (ROLL,PITCH,THROTTLE,YAW)
 #        self.vehicle.channels.overrides = {'1': ROLL,'2': PITCH,'3': THROTTLE,'4': YAW}
+
+    def saturate(self, value, minimum, maximum):
+        out = max(value,minimum)
+        out = min(out,maximum)
+        #print out
+        return out
+
+    #This function is used to clear commands being sent to the pixhawk.
+#    def releaseControl(self):
+#        self.vehicle.channels.overrides = {}
