d15df764e6b641e3371133cfd8b6ef87d12b2fe4
zlipp3032 <zlipp3032@gmail.com>
2018-06-05 20:39:34 -0400

    Up to date code using SOLO wifi connection and GPS

---
+++ SOLO_Flight_UDP/mainProgram_log.py
@@ -0,0 +1,85 @@
+from dronekit import connect, VehicleMode
+import sys
+import time
+import argparse
+import socket
+import os
+import Queue
+import threading
+#import receiveClass_log,loggingClass_log, callData_log
+import loggingClass_log, callData_log
+from defaultParams_log import *
+from rigidBodyState_log import *
+import numpy as np
+from datetime import datetime
+
+#Define the Agent on the network
+localIP = '192.168.0.7'
+Port = 5001
+myAddr = (localIP,Port)
+bufferLength = 1000
+expectedMAVs = 1
+logPath = 'C:\Users\Zack\Desktop\MastersThesisUAS\SOLOCode\Code\ThomasChanOverTests-PD/Data'
+#broadcastIP = '192.168.0.255' #Will be necessary when we introduce multiple agents
+#transmitAddr = (broadcastIP,Port)
+#d = 500
+
+startTime = datetime.now()
+
+
+#Create Message Queues
+#receiveQueue = Queue.Queue()
+logQueue = Queue.Queue()
+
+# Connect to the Pixhawk 2.0 Cube
+#         - It should be noted that the connection string will need to be
+#           modified pending the connect type
+#             + Connect string used for wireless connection to SOLO
+#                 'udpin:0.0.0.0:14550'
+#             + Connect string used for serial2 connection to SOLO via UP-Board
+#                 'dev/ttyAMA0' (Note the baudrate needs to be adjusted,
+#                 baud = 57600 for this connectino string)
+# Setup option parsing to get connection string
+parser = argparse.ArgumentParser(description='Commands vehicle using simple.')
+parser.add_argument('--connect',
+                      help="Vehicle connect target string. If not specified, SITL automatic")
+args = parser.parse_args()
+connection_string = args.connect
+sitl = None
+# Start SITL if no connection string specifiied
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+# Connect to UDP endpoint (and wait for defualt attributes to accumulate)
+print '\nConnecting to vehicle on: %s' % connection_string
+vehicle = connect(connection_string, wait_ready = True)#, baud=57600) #230400 or 115000 or 250000)
+
+
+#receiveThread = receiveClass_log.Receiver(receiveQueue,localIP,Port,bufferLength)
+logThread = loggingClass_log.Logging(logQueue,logPath,expectedMAVs,startTime) 
+controlThread = callData_log.Control(logQueue,startTime,localIP,defaultParams,vehicle) # IF you end up receiving data, you will need to add 'receiveQueue' to the items input into this class
+
+threads = []
+#threads.append(receiveThread)
+threads.append(logThread)
+threads.append(controlThread)
+
+#receiveThread.start()
+logThread.start()
+controlThread.start()
+
+def hasLiveThreads(threads):
+    return True in [t.isAlive() for t in threads]
+
+while hasLiveThreads(threads):
+    try:
+        [t.join(1) for t in threads
+        if t is not None and t.isAlive()]
+
+    except KeyboardInterrupt:
+        print "Killing Threads"
+        for t in threads:
+            t.stop()
+
+print "Exiting mainProgram"
---
+++ SOLO_Flight_UDP/mutil_log.py
@@ -0,0 +1,96 @@
+import sys
+from rigidBodyState_log import *
+from datetime import datetime
+
+
+
+	
+def vsToCSV(vs):
+#	print vs.ID
+#	print vs.command
+	out=''
+	out+=str(vs.ID+1) + ','
+#	if(vs.isFlocking):
+#		out+= '1,'
+#	else:
+#		out+= '0,'
+#   # out+=str(vs.test.xx)+','
+#  #  out+=str(vs.test.yy)+','
+# #   out+=str(vs.test.zz)+','
+##    out+=str(vs.test.qq)+','
+	out+=str(vs.test.roll)+','
+	out+=str(vs.test.pitch)+','
+	out+=str(vs.test.throttle)+','
+	out+=str(vs.test.yaw)+','
+        out+=str(vs.velocity.vx)+','
+	out+=str(vs.velocity.vy)+','
+	out+=str(vs.velocity.vz)+','
+        out+=str(vs.attitude.roll)+','
+        out+=str(vs.attitude.pitch)+','
+        out+=str(vs.attitude.yaw)+','
+        out+=str(vs.position.x)+','
+        out+=str(vs.position.y)+','
+        out+=str(vs.position.z)+','
+        #out+=str(vs.leader.gx),','
+        #out+=str(vs.leader.gy),','
+        #out+=str(vs.leader.gz),','
+        try:
+                out+=str(vs.command.ux)+','
+                out+=str(vs.command.uy)+','
+                out+=str(vs.command.uz)+','
+                out+=str(vs.command.vel_est_x)+','
+                out+=str(vs.command.vel_est_y)+','
+                out+=str(vs.command.vel_est_z)+','
+                out+=str(vs.command.Roll)+','
+                out+=str(vs.command.Pitch)+','
+                out+=str(vs.command.Throttle)+','
+                out+=str(vs.command.Yaw)+','
+        except (KeyboardInterrupt, SystemExit):
+                raise
+	#print (vs.position.keys['long])
+#	out+=str(vs.position)+','
+#	out+=str(vs.position.lon)+','
+#	out+=str(vs.position.alt)+','
+	#print (vs.velocity)
+#	out+=str(vs.velocity[0])+','
+#	out+=str(vs.velocity[1])+','
+#	out+=str(vs.velocity[2])+','
+#		out+=' ,'
+	
+#	try:
+#		out+=str((vs.timeout.peerLastRX[1]-epoch) . total_seconds())+','
+#	except: 
+#		out+=' ,'
+#	try: 
+#		out+=str((vs.timeout.peerLastRX[2]-epoch) . total_seconds())+','
+#	except:
+#		out+=' ,'
+#	try:
+#		out+=str((vs.timeout.peerLastRX[3]-epoch) . total_seconds())+','
+#	except:
+#		out+=' ,'
+
+#	try:
+#		out+=str(vs.abortReason) + ','
+#	except:
+#		out+=' ,'
+#	try:
+#		out+=str(vs.command.headingRate)+','
+#		out+=str(vs.command.climbRate)+','
+#		out+=str(vs.command.airSpeed)+','
+#		out+=str(vs.command.thetaD)+','
+#		out+=str(vs.command.accAltError)+','
+#	except (KeyboardInterrupt, SystemExit):
+#		raise
+#	except:
+#		out+=' , , , , ,'
+#	out+=str(vs.channels['1']) +', '+ str(vs.channels['2']) +','+ str(vs.channels['3'])+','+str(vs.channels['5'])+','+str(vs.channels['6'])+','
+#	out+=str(vs.servoOut['1'])+', ' +str(vs.servoOut['2'])+', ' + str(vs.servoOut['3'])
+	return out
+	
+def msgToCSVHeaders():
+	str = "Time,ID,roll,pitch,yaw,lat,lon,alt,latSpd,lngSpd,altSpeed,lastRXGnd,lastRX1,lastRX2,lastRX3, headingRateCmd, climbRateCmd, airSpeedCmd,ch1,ch2,ch3,ch5,ch6"
+	return str
+def vsToCSVHeaders():
+	str = "ID,isFlocking,roll,pitch,yaw,rollspeed,pitchspeed,yawspeed,lat,lon,alt,latSpd,lngSpd,altSpeed,airspeed,heading,headingRate,thetaDDotApprox,lastRXGnd,lastRX1,lastRX2,lastRX3,abortReason, headingRateCmd, climbRateCmd, airSpeedCmd,thetaD,accAltError,ch1,ch2,ch3,ch5,ch6,servo1,servo2,servo3"
+	return str
