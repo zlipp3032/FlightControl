a0c13c8a7ec88b9d0e9afe08f7697c86822ea739
zlipp3032 <zlipp3032@gmail.com>
2018-11-27 23:28:45 -0500

    broken code

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from dronekit import connect, VehicleMode
 import sys
 import time
-import argparse
 import socket
 import os
 import Queue
-import threading
+import multiprocessing
 import receiveClass_v5,loggingClass, controlSimple_v2
-from defaultParams import *
-#import receive, control, log
-from rigidBodyState import *
+import defaultConfig
+import rigidBodyState
+
 import numpy as np
 from datetime import datetime
+import argparse
+
 
 #Define the Agent on the network
 localIP = '192.168.2.1'
@@ -23,13 +24,17 @@ logPath = '/home/zsl/Desktop/multiFlightProgram/Data'
 #broadcastIP = '192.168.0.255' #Will be necessary when we introduce multiple agents
 #transmitAddr = (broadcastIP,Port)
 
+defaultParams = defaultConfig.getParams()
+
 startTime = datetime.now()
 
 #Create Message Queues
-receiveQueue = Queue.Queue()
-logQueue = Queue.Queue()
+receiveQueue = multiprocessing.Queue()
+logQueue = multiprocessing.Queue()
 
-# Connect to the Pixhawk 2.0 Cube
+
+
+# Connect to the Pixhawk 2.0 Cube (3DR SOLO Version)
 #         - It should be noted that the connection string will need to be
 #           modified pending the connect type
 #             + Connect string used for wireless connection to SOLO
@@ -49,13 +54,24 @@ if not connection_string:
     import dronekit_sitl
     sitl = dronekit_sitl.start_default()
     connection_string = sitl.connection_string()
+    
 # Connect to UDP endpoint (and wait for defualt attributes to accumulate)
 print '\nConnecting to vehicle on: %s' % connection_string
 vehicle = connect(connection_string, wait_ready = True, baud=115200, rate=10) #230400 or 115000 or 250000)
 
+vehicle.parameters['SR0_POSITION'] = 20
+vehicle.parameters['SR0_RC_CHAN'] = 20
+vehicle.parameters['SR0_EXTRA1'] = 20
+
+
 receiveThread = receiveClass_v5.Receiver(receiveQueue,localIP,Port,bufferLength)
+
+fileSuffix = '_v' + str(int(vehicle.parameters['SYSID_THISMAV'])) # This may only be applicable when outdoors???
 logThread = loggingClass.Logging(logQueue,logPath,expectedMAVs,startTime)
+
 controlThread = controlSimple_v2.Control(logQueue,receiveQueue,startTime,localIP,defaultParams,vehicle)
+
+print "default parameters" + str(defaultParams)
 
 threads = []
 threads.append(receiveThread)
